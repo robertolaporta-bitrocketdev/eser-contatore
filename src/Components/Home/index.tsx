b42652c004516a0803cb673cf8439fcdc0f6309f
Roberto La Porta <robertolaporta@MacBook-Pro-di-Roberto.local>
2022-04-05 17:52:18 +0200

    feat(router): routing preferiti

---
+++ src/Components/Home/index.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+
+export const Home: FC = (): JSX.Element => {
+  return (
+    <div>
+      <Link to="counter">
+        <button>Contatore</button>
+      </Link>
+      <Link to="fetching">
+        {" "}
+        <button>Fetching</button>
+      </Link>
+      <Link to="preferiti">
+        {" "}
+        <button>Preferiti</button>
+      </Link>
+    </div>
+  );
+};
