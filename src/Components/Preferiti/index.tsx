a57aa7d4f55bf7ec7a7cccdb6c19ba76b96962b8
Roberto La Porta <robertolaporta@MacBook-Pro-di-Roberto.local>
2022-04-07 11:59:01 +0200

    feat(userdata): add dispatch favourite

--- src/Components/Preferiti/index.tsx
+++ src/Components/Preferiti/index.tsx
@@ -1,19 +1,34 @@
 import { FC } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export const Preferiti: FC = (): JSX.Element => {
+  const dispatch = useDispatch();
   const city = useSelector((store: any) => store.city);
-
+  const userData = useSelector((store: any) => store.userData);
   return (
     <div className="preferitiBox">
       <h1>Preferiti</h1>
-      <p>Torniamo alla Home:</p>
       <Link to={"/"}>
         <button>HOME</button>
       </Link>
 
-      <p>`La più bella città del mondo è {city}`</p>
+      {/* <p>`La più bella città del mondo è {city}`</p> */}
+      <br />
+      <h2>Dati utente:</h2>
+      <p>Id: {userData.id}</p>
+      <p>Title: {userData.title}</p>
+      <p>completed: {JSON.stringify(userData.completed)}</p>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "INSERT_DATA",
+            payload: {},
+          })
+        }
+      >
+        Elimina
+      </button>
     </div>
   );
 };
